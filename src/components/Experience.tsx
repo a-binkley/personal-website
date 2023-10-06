7b138044ff6fb46e9b168a75e038a3883915d741
AdrianBinkley <adrianbinkley@gmail.com>
2023-10-06 14:37:15 -0400

    add bio info

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,21 @@
 import './styles/Experience.css';
 
+type ExperienceDetails = {
+    companyOrSchool: string;
+    fromDate: string;
+    toDate: string;
+    description: string;
+}
+
+const workExperience: ExperienceDetails[] = [
+    {
+        companyOrSchool: 'The Vanguard Group',
+        fromDate: 'May 2022',
+        toDate: 'Present',
+        description: 'TODO'
+    }
+]
+
 export function Experience() {
     return (
         <div id='experienceContainer'>
