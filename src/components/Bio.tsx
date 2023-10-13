377cbe294c4cebc2e6597f9d7773ef746cc0b7b0
AdrianBinkley <adrianbinkley@gmail.com>
2023-10-13 17:19:24 -0400

    rename resume on download

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,5 +1,6 @@
 import '../styles/Bio.css';
 import '../fonts/Proxima Nova/stylesheet.css';
+
 import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -29,7 +30,7 @@ Additionally, you can:`
                 'M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z', 
                 'M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z',
             ],
-            anchor: <Link to='Resume.pdf' target='_blank' className='bioLink' download>Download my Résumé</Link>
+            anchor: <Link to='Resume.pdf' target='_blank' className='bioLink' download='Adrian Binkley - Resume.pdf'>Download my Résumé</Link>
         // TODO: add back once CV finalized
         // }, {
         //     iconClass: 'download',
