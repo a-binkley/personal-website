4b424c4d4f7bebbdb75dbe658bc6f159dbb1487b
AdrianBinkley <adrianbinkley@gmail.com>
2023-10-16 19:19:19 -0400

    add API requests and data piping

--- src/components/pages/Portfolio.tsx
+++ src/components/pages/Portfolio.tsx
@@ -2,8 +2,9 @@ import { Outlet } from 'react-router-dom';
 
 import { AccentTab, PageMenu, SocialLink } from '..';
 import '../../styles/pages/Portfolio.css';
+import { PortfolioData } from '../..';
 
-export function Portfolio() {
+export function Portfolio(props: { data: PortfolioData[] | null }) {
     return (
         <>
             <AccentTab corner='NW' desktopOnly={true} />
@@ -11,7 +12,17 @@ export function Portfolio() {
             <h1 id='portfolio-header'>Portfolio</h1>
             <div className='mobile-title-sep' />
             <div id='portfolio-body-wrapper'>
-                <p>Content coming soon!</p>
+                {props.data ? <div id='portfolio-data-wrapper'>
+                    {props.data.map(({ title, tagline, lastModified }, index) => {
+                        return (
+                            <div className='portfolio-project' key={index}>
+                                <h2 className='portfolio-project-title'>{title}</h2>
+                                <h3 className='portfolio-project-tagline'>{tagline}</h3>
+                                <h4 className='portfolio-project-modified'>{lastModified}</h4>
+                            </div>
+                        );
+                    })}
+                </div> : <p>Fetching content...</p>}
                 <SocialLink href='https://github.com/a-binkley' />
             </div>
             <AccentTab corner='SE' desktopOnly={false} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,82 @@
-import ReactDOM from 'react-dom/client';
-import { About, Home, NotFound, Portfolio } from './components/pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { useEffect, useState } from 'react';
+import { Octokit } from 'octokit';
+import { Buffer } from 'buffer';
+import moment from 'moment';
 
+import { About, Home, NotFound, Portfolio } from './components/pages';
 import './index.css';
 import './styles/MobileTitleSeparator.css';
 import './fonts/Proxima Nova/stylesheet.css';
 
+export type PortfolioData = {
+  title: string;
+  tagline: string;
+  lastModified: string;
+};
+
 export default function App() {
+  const [portfolioData, setPortfolioData] = useState<PortfolioData[] | null>(null);
+
+  useEffect(() => {
+    /** Fetch repository info for portfolio page
+     * @param client the {@link Octokit} client used to make API calls
+     * @returns an array of {@link PortfolioData} objects to be used for rendering
+     */
+    const fetchGitHubData = async (client: Octokit) => {
+
+      // Retrieve all public repositories for a-binkley
+      const repos = await client.request('GET /users/{username}/repos', {
+        username: 'a-binkley',
+      });
+      
+      if (repos.status !== 200) {
+        console.error(`Unable to fetch general portfolio data from GitHub (error ${repos.status}). ${repos.data}`);
+      } else {
+        const data: PortfolioData[] = [];
+
+        // Check for repositories with a portfolio-tags.json and save those to render on Portfolio route
+        for (const repo of repos.data.slice(4, 6)) {  // STUB
+          const repoResponse = await client.request('GET /repos/{owner}/{repo}/contents/{path}', {
+            owner: 'a-binkley',
+            repo: repo.name,
+            path: 'portfolio-tags.json'
+          });
+
+          if (repoResponse.status !== 200) {
+            console.error(`Unable to fetch portfolio data for repository ${repo.name} (error ${repoResponse.status}). ${repoResponse.data}`);
+          } else if (Array.isArray(repoResponse.data) || repoResponse.data.type !== 'file') {
+            console.error(`Unable to parse portfolio-tags.json for repository ${repo.name}`);
+          } else {
+            // Parse portfolio tag data and add to array
+            const { title, tagline } = JSON.parse(Buffer.from(repoResponse.data.content, 'base64').toString());
+            data.push({
+              title,
+              tagline,
+              lastModified: repo.updated_at ? moment(repo.updated_at).format('ll') : 'No data'
+            });
+          }
+        }
+
+        setPortfolioData(data);
+      }
+    };
+
+    // Fetch portfolio data only if not already present
+    if (portfolioData === null) {
+      const octokit = new Octokit();
+      fetchGitHubData(octokit);
+    }
+  });
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"          element={<Home />} />
-        <Route path="/about"     element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/*"         element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio data={portfolioData} />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
