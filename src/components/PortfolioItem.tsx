d4b3e099d2994176cb35c08e222f5d17365f2394
AdrianBinkley <adrianbinkley@gmail.com>
2023-10-20 17:12:23 -0400

    style portfolio items

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -3,12 +3,14 @@ import '../styles/PortfolioItem.css';
 export function PortfolioItem(props: { title: string, tagline: string, lastModified: string, bootstrapIcon: string, iconPaths: string[], url: string }) {
     return (
         <a href={props.url} className='portfolio-item-wrapper' target='_blank' rel='noreferrer'>
-            <svg className={`bi bi-${props.bootstrapIcon}`} xmlns='http://www.w3.org/2000/svg' width='64' height='64' fill='currentColor' viewBox='0 0 16 16'>
+            <svg className={`bi bi-${props.bootstrapIcon} portfolio-icon`} xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 24 24'>
                 {props.iconPaths.map((iconPath, pathIndex) => <path d={iconPath} key={`portfolio-icon-path-${pathIndex}`} />)}
             </svg>
-            <h2 className='portfolio-item-title'>{props.title}</h2>
-            <h3 className='portfolio-item-tagline'>{props.tagline}</h3>
-            <h4 className='portfolio-item-modified'>{props.lastModified}</h4>
+            <div className='portfolio-item-info-wrapper'>
+                <h2 className='portfolio-item-title'>{props.title}</h2>
+                <h3 className='portfolio-item-tagline'>{props.tagline}</h3>
+                <h4 className='portfolio-item-modified'>{props.lastModified}</h4>
+            </div>
         </a>
     );
 }
