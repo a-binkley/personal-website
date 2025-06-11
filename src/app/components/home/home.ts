1601e074cf27a2249c8b4d27cf2e13a57116cedd
AdrianBinkley <adrianbinkley@gmail.com>
2025-06-11 11:33:09 -0400

    add skeleton for home, nav

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { home } from '../../../constants';
+import { HomeService } from '../../services/home';
+import { Nav } from '../shared/nav/nav';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Nav],
+  standalone: true,
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
+  public readonly content = home;
+  private readonly homeSvc = inject(HomeService);
 
+  public get yoe(): number {
+    return this.homeSvc.experienceYears;
+  }
 }
---
+++ src/constants/textBlocks.ts
@@ -0,0 +1,10 @@
+export const home = {
+  experience: [
+    "I'm a full-stack software engineer with over",
+    'of professional experience',
+  ],
+  nextSteps: [
+    'For more detailed information, you can download my resumé below or check out my',
+    'portfolio of side projects',
+  ],
+};
