a1511cc76249ad473b3acfcf69bd2b8bf2c68edd
AdrianBinkley <adrianbinkley@gmail.com>
2025-06-11 09:22:29 -0400

    set up routes, basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Bio } from './components/bio/bio';
+import { Contact } from './components/contact/contact';
+import { Home } from './components/home/home';
+import { Portfolio } from './components/portfolio/portfolio';
+
+export const routes: Routes = [
+  {
+    title: 'Home',
+    path: '',
+    component: Home,
+  },
+  {
+    title: 'Portfolio',
+    path: 'portfolio',
+    component: Portfolio,
+  },
+  {
+    title: 'Bio',
+    path: 'bio',
+    component: Bio,
+  },
+  {
+    title: 'Contact',
+    path: 'contact',
+    component: Contact,
+  },
+];
