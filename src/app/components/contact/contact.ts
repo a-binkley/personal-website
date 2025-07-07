de6d320e8c28585f3a9122f5c9992a0c2c1c6ab9
a-binkley <adrianbinkley@gmail.com>
2025-07-06 20:46:40 -0400

    feat(contact): add contact page

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,12 +1,44 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Toast } from 'primeng/toast';
 
+import { COLORS } from '../../../constants';
 import { Nav } from '../shared/nav/nav';
 
 @Component({
   selector: 'app-contact',
-  imports: [Nav],
+  imports: [ButtonModule, Nav, Toast],
+  providers: [MessageService],
   standalone: true,
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+  constructor (private clipboard: Clipboard, private msgSvc: MessageService) {}
+
+  public copyEmail() {
+    this.clipboard.copy('adrianbinkley@gmail.com');
+    this.msgSvc.add({
+      severity: 'info',
+      summary: 'Copied',
+      detail: 'Email address copied to clipboard',
+      life: 3000
+    });
+  }
+
+  public readonly emailCopyBtn = {
+    colorScheme: {
+      light: {
+        root: {
+          primary: {
+            color: COLORS.ACCENT,
+            background: COLORS.PRIMARY,
+            borderColor: COLORS.ACCENT,
+          },
+        },
+      },
+    },
+  };
+}
